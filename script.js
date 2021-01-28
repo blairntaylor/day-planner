1798de486c03ae7af9dc5581053ad8693e42f2f4
Blair Taylor <blairntaylor@gmail.com>
2021-01-28 09:02:50 -0700

    Update JS for storing values and changeHour function

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
 
+//save button and var for text and hour
 $(".saveBtn").on("click", function(){
     var saveText = $(this).siblings(".description").val()
     var saveHour = $(this).parent().attr("id")
     localStorage.setItem(saveHour, saveText)
 
 })
+
+function changeHour (){
+ var currentTime = moment().hours()
+ $(".time-block").each(function(){
+     var hourNum = parseInt($(this).attr("id").split("-")[1])
+    if (hourNum < currentTime){
+        $(this).addClass("past")
+    }
+    else if (hourNum === currentTime){
+        $(this).removeClass("past")
+        $(this).addClass("present")
+    }
+    else {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
+    }
+ })
+
+}
+
+changeHour()
+
+
+var interval = setInterval(changeHour, 15000)
 
 //retrieving information
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
@@ -18,6 +44,6 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"))
 $("#hour-16 .description").val(localStorage.getItem("hour-16"))
 $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-
+$("#currentDay").text(moment.js) //moment in time
 
 })
